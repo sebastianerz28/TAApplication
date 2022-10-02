ddddeec522546512f69b8bc458b79dcff26c38f5
sebastianerz28 <sebastianerz@gmail.com>
2022-10-02 14:41:03 -0600

    Move view to OLD, Create Application model

---
+++ TAApplication/TAApplication/Controllers/OLDController.cs
@@ -0,0 +1,103 @@
+﻿/**
+ * Author:    Sebastian Ramirez
+ * Partner:   Noah Carlson
+ * Date:      Sept 27, 2022
+ * Course:    CS 4540, University of Utah, School of Computing
+ * Copyright: CS 4540 and Noah Carlson/Sebastian Ramirez - This work may not be copied for use in Academic Coursework.
+ *
+ * I, Sebastian Ramirez, certify that I wrote this code from scratch and did 
+ * not copy it in part or whole from another source.  Any references used 
+ * in the completion of the assignment are cited in my README file and in
+ * the appropriate method header.
+ *
+ * File Contents
+ *
+ *  This file serves as the controller for the home page returning appropriate views for other pages
+ *    
+ */
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using TAApplication.Areas.Data;
+using TAApplication.Models;
+
+namespace TAApplication.Controllers
+{
+    [Authorize]
+    public class OLDController : Controller
+    {
+        private readonly ILogger<OLDController> _logger;
+        UserManager<TAUser> _um;
+
+        public OLDController(ILogger<OLDController> logger, UserManager<TAUser> um)
+        {
+            _logger = logger;
+            _um = um;
+        }
+
+        /// <summary>
+        /// Returns the home page
+        /// Accessible by any user regardless of login status
+        /// </summary>
+        /// <returns> Home Page </returns>
+
+        /// <summary>
+        /// Returns the privacy view
+        /// Only accessible by logged in user
+        /// </summary>
+        /// <returns> Privacy Page </returns
+
+        /// <summary>
+        /// Returns the page to create an application
+        /// Only Accessible by Applicants
+        /// </summary>
+        /// <returns> Application Create Page </returns>
+        public IActionResult ApplicationCreate()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Returns the Applicant List
+        /// Only Accesible by professors and admins
+        /// </summary>
+        /// <returns> ApplicantList Page</returns
+        public IActionResult ApplicantList()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// Returns Application Details
+        /// Only Accesible by professors, admins, and specific user u0000000
+        /// </summary>
+        /// <returns> ApplicationDetails Page </returns>
+        
+        public IActionResult ApplicationDetails()
+        {
+            //Check if it is user u0000000
+            /*
+            if (_um.GetUserAsync(User).Result.Unid != "u0000000" && _um.GetRolesAsync(_um.GetUserAsync(User).Result).Result.FirstOrDefault().Equals("Applicant"))
+            {
+                return View("NotAuthorized");
+            }
+            else
+            {
+                return View();
+            }
+            */
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ TAApplication/TAApplication/Models/Application.cs
@@ -0,0 +1,65 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using TAApplication.Areas.Data;
+
+namespace TAApplication.Models
+{
+    public enum DegreePursuing { BS, Masters, BSMS, PhD }
+    public class Application
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "The degree being pursued", ShortName = "Degree Pursuing",Prompt ="BS", Description ="This data will help us assign you to the appropriate level of courses e.g(2xxx) level")]
+        public DegreePursuing DegreePursuing { get; set; }
+        [Required]
+        [Display(Name = "The department that hosts your major", ShortName = "Department", Prompt = "CS, CE", Description = "This data will help us assign you to the correct type of classes")]
+        [StringLength(150)]
+        public string Department { get; set; }
+        [Required]
+        [Display(Name = "UofU GPA", ShortName = "GPA", Prompt = "4.0", Description = "This is your current cumulative GPA.")]
+        [Range(0,5)]
+        public double GPA { get; set; }
+        [Required]
+        [Display(Name = "Desired Weekly Hours", ShortName = "Desired Hours", Prompt = "e.g. 6", Description = "This helps determine scheduling.")]
+        [Range(5,20)]
+        public int DesiredHours { get; set; }
+        [Required]
+        [Display(Name = "Avalability before semester?", ShortName = "Available", Prompt = "True", Description = "This is whether you will be able to start the week before school starts.")]
+        public bool AvailableBeforeSemester { get; set; }
+        [Required]
+        [Display(Name = "Semesters Completed at UofU", ShortName = "SemestersCompleted", Prompt = "e.g. 3", Description = "This is used to determine how many years the student has been at the university")]
+        [Range(0,int.MaxValue)]
+        public int SemestersCompleted { get; set; }
+        [Required]
+        [StringLength(50000)]
+        [Display(Name = "Personal Statement", ShortName = "PersonalStatement", Prompt = "Some information about your self.", Description = "This is a place where you can share who you are outside of the gpa. Let us know anything you deem important.")]
+        [DataType(DataType.MultilineText)]
+        [DisplayFormat(NullDisplayText ="Not provided")]
+        public string? PersonalStatement { get; set; }
+        [Display(Name = "School Transferred From", ShortName = "Transfer School", Prompt = "University of California Berkeley", Description = "Please let us know the school that you have transferred from if applicable")]
+        [StringLength(150)]
+        [DisplayFormat(NullDisplayText = "Not Provided")]
+        public string? TransferSchool { get; set; }
+        [Display(Name = "Applicants LinkedIn profile", ShortName = "LinkedIn", Prompt = "https://www.linkedin.com/", Description = "This links to the applicants LinkedIn profile")]
+        [DisplayFormat(NullDisplayText = "Not Provided")]
+        [Url]
+        public string? LinkedIn { get; set; }
+        [Display(Name = "Resume", ShortName = "Resume", Prompt = "Insert a personal resume", Description = "This is where you can input your personal resume to give more background on your accomplishments.")]
+        [DisplayFormat(NullDisplayText = "Not Provided")]
+        [RegularExpression("^[^] +\\.pdf$", ErrorMessage ="File must be a pdf")]
+        public string? ResumeName { get; set; }
+        [Required]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy hh:mm:ss tt}")]
+
+        public DateTime CreationDate { get; set; }
+        [Required]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy hh:mm:ss tt}")]
+        public DateTime ModificationDate { get; set; }
+        [Required]
+        public TAUser Applicant { get; set; }
+        
+    }
+}
