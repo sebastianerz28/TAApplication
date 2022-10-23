2613a9621205711969f48e0f0fb63e485fe70519
Smirqy <noahrk14@gmail.com>
2022-10-23 12:09:25 -0600

    Some popup changes

---
+++ TAApplication/TAApplication/wwwroot/js/Course/Course.js
@@ -0,0 +1,18 @@
+﻿
+function popup() {
+    $("#popupwindow").modal("show");
+};
+
+function EditNote(Course_ID, Note) {
+    $.post(
+        {
+            url: "/Courses/EditNote",
+            data: {
+                id: id,
+                contents: contents,
+            }
+        })
+        .done(function (data) {
+            console.log("Sample of data:", data);
+        });
+}
