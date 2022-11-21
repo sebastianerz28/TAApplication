87b0aadf42591bbe1556bbcea35dade7b74c3c6a
sebastianerz28 <sebastianerz@gmail.com>
2022-11-21 04:06:24 -0700

    Add save schedule functionality, fix unsynchronized drawing

--- TAApplication/TAApplication/wwwroot/js/Availability/availability.js
+++ TAApplication/TAApplication/wwwroot/js/Availability/availability.js
@@ -9,6 +9,8 @@ $("#canvas_div").on('mouseup', function (e) {
     mousedown = false;
 });
 
+var slots = [];
+
 class Slot extends PIXI.Graphics {
     on_color = 0xbb500;
     off_color = 0xbbbbbb;
@@ -54,11 +56,13 @@ class Slot extends PIXI.Graphics {
         if (typeselected == this.selected) {
             this.selected = !this.selected;
             if (typeselected) {
+                slots[this.id] = "0";
                 this.clear();
                 this.beginFill(this.off_color);
                 this.drawRect(0, 0, this.width, this.height);
             }
             else {
+                slots[this.id] = "1";
                 this.clear();
                 this.beginFill(this.on_color);
                 this.drawRect(0, 0, this.width, this.height);
@@ -67,14 +71,58 @@ class Slot extends PIXI.Graphics {
     }
 
     init_draw(isSelected) {
+        
         this.clear();
         if (isSelected) {
+            slots.push("1");
             this.beginFill(this.on_color);
         }
         else {
+            slots.push("0");
             this.beginFill(this.off_color);
         }
         this.drawRect(0, 0, this.width, this.height);
     }
 
+}
+
+
+function drawDays() {
+    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+    let dayX = 100;
+    for (let i = 0; i < days.length; i++) {
+        var dayText = new PIXI.Text(days[i],
+            {
+                fontFamily: 'Arial',
+                fontSize: 14,
+                fill: 0x000000,
+                align: 'center',
+            });
+
+        app.stage.addChild(dayText)
+        // Position the text
+        dayText.x = dayX + 15;
+        dayText.y = 25;
+        dayX += 110;
+    }
+    console.log(slots);
+}
+
+
+function Save_Availability(userId) {
+    let data = "";
+    for (let i = 0; i < slots.length; i++)
+        data += slots[i];
+    console.log
+    $.post(
+            {
+                url: "/Availabilities/SetSchedule",
+                data: {
+                    slots: data,
+                    userId: userId,
+                }
+            })
+        .done(function (data) {
+            console.log("Sample of data:", data);
+        });
 }
