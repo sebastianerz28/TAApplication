d6c3f9cddc942776486a1baab817e00c4edb3681
Smirqy <noahrk14@gmail.com>
2022-11-22 16:18:40 -0700

    Updated JS header comments

--- TAApplication/TAApplication/wwwroot/js/Availability/availability.js
+++ TAApplication/TAApplication/wwwroot/js/Availability/availability.js
@@ -1,4 +1,20 @@
-﻿let mousedown = false;
+﻿/**
+ * Author:    Sebastian Ramirez
+ * Partner:   Noah Carlson
+ * Date:      11/22/2022
+ * Course:    CS 4540, University of Utah, School of Computing
+ * Copyright: CS 4540 and [Noah Carlson/Sebastian Ramirez] - This work may not be copied for use in Academic Coursework.
+ *
+ * I, Sebastian Ramirez and Noah Carlson, certify that I wrote this code from scratch and did 
+ * not copy it in part or whole from another source.  Any references used 
+ * in the completion of the assignment are cited in my README file and in
+ * the appropriate method header.
+ *
+ * File Contents
+ *      This File contains the java script for the availability PIXI web application's functionality.
+ */
+
+let mousedown = false;
 var typeselected = false;
 
 $("#canvas_div").on('mousedown', function (e) {
--- TAApplication/TAApplication/wwwroot/js/Availability/setup.js
+++ TAApplication/TAApplication/wwwroot/js/Availability/setup.js
@@ -1,11 +1,21 @@
 ﻿/**
- * Global access to the PIXI Application 
+ * Author:    Sebastian Ramirez
+ * Partner:   Noah Carlson
+ * Date:      11/22/2022
+ * Course:    CS 4540, University of Utah, School of Computing
+ * Copyright: CS 4540 and [Noah Carlson/Sebastian Ramirez] - This work may not be copied for use in Academic Coursework.
+ *
+ * I, Sebastian Ramirez and Noah Carlson, certify that I wrote this code from scratch and did 
+ * not copy it in part or whole from another source.  Any references used 
+ * in the completion of the assignment are cited in my README file and in
+ * the appropriate method header.
+ *
+ * File Contents
+ *      This File contains the java script for the setup of the availability PIXI web application's functionality.
  */
+
 var app = null;
 
-/**
- *  Create PIXI stage
- */
 function setup_pixi(width, height) {
     let bg_color = 0xFFFFFF;
     app = new PIXI.Application({ backgroundColor: bg_color });
